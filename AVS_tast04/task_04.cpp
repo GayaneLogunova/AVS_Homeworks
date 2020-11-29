908fa95b63fe761e18000626d98b822d440d1819
GayaneLogunova <65052316+GayaneLogunova@users.noreply.github.com>
2020-11-29 21:11:03 +0300

    Add files via upload

---
+++ AVS_tast04/task_04.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int desicion = -1;
+// вариант 19
+
+
+void server(int N) {
+	int check_desicion;
+	do {
+		#pragma omp critical 
+		{
+		cout << "Введите № выбранной валентинки: ";
+		}
+		cin >> check_desicion;
+		cout << endl;
+		if (check_desicion > N) {
+			cout << "Номер выбранной валентинки не может превышать общее количесвто валентинок: " << endl;
+		}
+	} while (check_desicion > N);
+	desicion = check_desicion;
+}
+
+void client(int my_number) {
+
+	while (desicion == -1) {
+		continue;
+	}
+	if (desicion != my_number) {
+		#pragma omp critical
+		{
+			cout << "Валентинка №" << my_number << " отклонена" << endl;
+		}
+	}
+	else {
+		#pragma omp critical 
+		{
+			cout << "валентинка №" << my_number << " принята" << endl;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int N;
+	cout << "Введите количество валентинок N: ";
+	cin >> N;
+	
+	server(N);
+	
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++) {
+		client(i);
+	}
+}
+
